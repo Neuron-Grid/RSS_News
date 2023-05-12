1df919d8312e4e9593c27d5a342ae0f62894d569
Neuron Grid <neuron-grid@neuron-grid.net>
2023-05-12 11:22:28 +0900

    requirements.txtなど一部を更新

--- RSS_News/settings.py
+++ RSS_News/settings.py
@@ -22,7 +22,6 @@ env.read_env(os.path.join(BASE_DIR, 'service.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -139,7 +138,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
